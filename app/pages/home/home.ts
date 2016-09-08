f991cddf8cd7155bfc56c63f3b096d3931f866c6
masteroren <master.oren@gmail.com>
2016-09-09 00:54:33 +0300

    'message'

---
+++ app/pages/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from 'ionic-native';
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  scanText: string;
+
+  constructor(private navCtrl: NavController) {
+  
+  }
+
+  scan(){
+
+    this.scanText = "Scaning...";
+
+    BarcodeScanner.scan().then((barcodeData) => {
+      this.scanText = "Scan Finished";
+    }, (err) => {
+        this.scanText = "Scan error: " + err;
+    });
+  }
+}
